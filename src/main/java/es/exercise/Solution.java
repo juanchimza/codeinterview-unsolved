1387113cc0dafee897588cab94e3f71a012e025b
Juan Marziali <juanchimza@gmail.com>
2020-11-02 12:42:03 -0300

    A simpler way

--- src/main/java/es/exercise/Solution.java
+++ src/main/java/es/exercise/Solution.java
@@ -1,11 +1,14 @@
 package es.exercise;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import es.exercise.pojo.ProductSales;
 import es.exercise.pojo.ProductStock;
-
-import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Solution {
 
@@ -14,39 +17,27 @@ public class Solution {
                                                   List<ProductSales> productsSalesInformation) {
 
     	Map<Long, Double> ratedProducts = new HashMap<>();
-    	List<Long> productsSortedByScores = new ArrayList<>();
-    	List<Double> scores = new ArrayList<>();
+    	List<Long> idProductsSortedByScores = new ArrayList<>();
     	
     	try {
     	
-    		ratedProducts = productsStockInformation.stream().collect(Collectors.toMap(ProductStock::getProductId,stock -> Double.valueOf(stock.getAvailableStock()*stockWeight/100)));
+    		ratedProducts = productsStockInformation.parallelStream().collect(Collectors.toMap(ProductStock::getProductId,stock -> Double.valueOf((stock.getAvailableStock()*stockWeight))));
     		
     		for (ProductSales product : productsSalesInformation) {
-    			ratedProducts.computeIfPresent(product.getProductId(), (key, value) -> value+product.getSalesAmount()*salesWeight/100);
+    			ratedProducts.computeIfPresent(product.getProductId(), (key, value) -> value+product.getSalesAmount()*salesWeight);
 			}
     		
-    		scores = ratedProducts.values().stream().collect(Collectors.toList());
+			Map<Long, Double> productsSortedByScores = ratedProducts.entrySet().stream()
+    				  .sorted(Map.Entry.<Long, Double>comparingByValue().reversed())
+    				  .collect(Collectors.toMap(Map.Entry::getKey,Map.Entry::getValue,(unordered, ordered) -> unordered, LinkedHashMap::new));
     		
-    		while (productsSortedByScores.size()<productsStockInformation.size()) {
-    			Double maxValue = scores.stream()
-    			      .mapToDouble(v -> v)
-    			      .max().orElseThrow(NoSuchElementException::new);
-    			
-    			Long key = ratedProducts
-    				      .entrySet()
-    				      .stream()
-    				      .filter(entry -> maxValue.equals(entry.getValue()))
-    				      .map(Map.Entry::getKey).findFirst().get();
-    			
-    			productsSortedByScores.add(key);
-    			scores.remove(maxValue);
-			}
+			idProductsSortedByScores = productsSortedByScores.keySet().stream().collect(Collectors.toCollection(ArrayList::new));
     		
     	} catch (Exception e) {
     		e.printStackTrace();
 		} 
     	
-    	return productsSortedByScores;
+    	return idProductsSortedByScores;
     }
     
 }
